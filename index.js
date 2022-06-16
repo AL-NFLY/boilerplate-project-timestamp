4b0934344c0783dc6115f1bc3f8057ecc1beda1d
AL-NFLY <almernfly@gmail.com>
2022-06-16 19:28:18 +0700

    timestamp

--- index.js
+++ index.js
@@ -2,31 +2,52 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+let cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(3003, () => {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let resObj = {};
+app.get('/api/:date', (req, res) => {
+  let date = req.params.date;
+  if (date.includes('-')) {
+    resObj['unix'] = new Date(date).getTime();
+    resObj['utc'] = new Date(date).toUTCString();
+  } else {
+    date = parseInt(date);
+    resObj['unix'] = new Date(date).getTime();
+    resObj['utc'] = new Date(date).toUTCString();
+  }
+  if (!resObj['unix'] || resObj['utc']) {
+    res.json({ error: 'Invalid Date' });
+  }
+
+  res.json(resObj);
+});
+
+app.get('/api', (req, res) => {
+  resObj['unix'] = new Date().getTime();
+  resObj['utc'] = new Date().toUTCString();
+  res.json(resObj);
 });
